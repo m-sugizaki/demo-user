8f977bd0dbb14e91557520fdac203a77bc475541
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-05-22 11:38:02 +0900

    rest-assured

--- src/test/java/com/example/api/UserRestControllerIntegrationTest.java
+++ src/test/java/com/example/api/UserRestControllerIntegrationTest.java
@@ -1,32 +1,85 @@
 package com.example.api;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.net.URI;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.server.LocalServerPort ;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
+import io.restassured.RestAssured;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+import static io.restassured.matcher.ResponseAwareMatcher.*;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
-public class UserRestControllerIntegrationTest{
-    @LocalServerPort
-    private int port;
+public class UserRestControllerIntegrationTest {
 
-    @Autowired
-    private TestRestTemplate template;
+	private static String postJson = "{\"id\": 9, \"name\": \"Test Taro\", \"pass\": \"testtaro\"}";
+	private static String putJson = "{\"id\": 9, \"name\": \"Update Taro\", \"pass\": \"updatetaro\"}";
+	
+    @Test
+    public void メンバー一覧() {
+        RestAssured.baseURI = "http://13.115.40.246:8081";
+        given()
+        .get("/users/")
+        .then()
+        	.body("id[0]", equalTo(1))
+        	.body("name[0]", equalTo("杉崎　睦"))
+        	.body("pass[0]", equalTo("sugisugi"))
+        	.body("id[1]", equalTo(2))
+        	.body("name[1]", equalTo("中森健"))
+        	.body("pass[1]", equalTo("01234567"))
+        	// .log().body()
+        ;
+    }
 
     @Test
-    public void 一覧取得() throws Exception {
-        URI uri = new URI("http://localhost:" + port + "/users/");
-        String responseBody = template.getForObject(uri, String.class);
-        assertThat(responseBody).isEqualTo("[{\"id\":1,\"name\":\"杉崎　睦\",\"pass\":\"sugisugi\"},{\"id\":2,\"name\":\"中森健\",\"pass\":\"01234567\"},{\"id\":3,\"name\":\"Sato\",\"pass\":\"satosato\"},{\"id\":4,\"name\":\"Suzuki\",\"pass\":\"suzukima\"},{\"id\":5,\"name\":\"鈴木太郎\",\"pass\":\"tarotaro\"}]");
+    public void メンバー詳細() {
+        RestAssured.baseURI = "http://13.115.40.246:8081";
+        given()
+        .get("/users/1")
+        .then()
+            .body("id", equalTo(1))
+            .body("name", equalTo("杉崎　睦"))
+            .body("pass", equalTo("sugisugi"))
+         	// .log().body()
+        ;
+    }
+
+    @Test
+    public void メンバー登録() {
+        RestAssured.baseURI = "http://13.115.40.246:8081";
+        given()
+        .contentType("application/json\r\n")
+        .body(postJson) // Request Body の設定
+        .post("/users")
+        .then()
+            .body("id", equalTo(9))
+        	.body("name", equalTo("Test Taro"))
+        	.body("pass", equalTo("testtaro"))
+            // .log().body()
+        ;
+    }
+
+    @Test
+    public void メンバー更新() {
+        RestAssured.baseURI = "http://13.115.40.246:8081";
+        given()
+        .contentType("application/json\r\n")
+        .body(putJson) // Request Body の設定
+        .put("/users/9")
+        .then()
+        	.body("id", equalTo(9))
+        	.body("name", equalTo("Update Taro"))
+        	.body("pass", equalTo("updatetaro"))
+           	// .log().body()
+        ;
+    }
+
+    @Test
+    public void メンバー削除() {
+        RestAssured.baseURI = "http://13.115.40.246:8081";
+        given()
+        .contentType("application/json\r\n")
+        .delete("/users/9")
+        .then()
+        	.statusCode(204)
+        	// .log().all()
+        ;
     }
 }
