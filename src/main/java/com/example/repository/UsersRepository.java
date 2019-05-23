b36c777883d23503c496ced2287365b366578140
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-05-23 14:28:11 +0900

    aaa

--- src/main/java/com/example/repository/UsersRepository.java
+++ src/main/java/com/example/repository/UsersRepository.java
@@ -7,7 +7,6 @@ import com.example.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository; //
 
-
 @Repository //
 public interface UsersRepository extends JpaRepository<User, Integer> {
   public User findById(int id);
